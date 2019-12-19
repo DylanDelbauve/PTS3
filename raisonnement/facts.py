e9ad8342f4569e76e53177567d7e7ee371e8d984
Thomas Bénas <akgx99@gmail.com>
2019-12-20 00:07:52 +0100

    facts cuisine

--- raisonnement/facts.py
+++ raisonnement/facts.py
@@ -1,11 +1,8 @@
 from pyswip import Prolog
 prolog = Prolog
-
-#cusine
-prolog.asserta("mur(4)") # nombre de mur
-prolog.asserta("ouverture(porte)") # il y a une porte
-prolog.asserta("ouverture(fenetre)") # il y a une fenetre
-prolog.asserta("desservi(electricite)") # il y a un acces a l'electricite
-prolog.asserta("desservi(chauffage)") # il y a un acces au chauffage
-prolog.asserta("desservi(gaz)") # il y a un un acces au gaz
-prolog.asserta("superficie(9)") # La supérficie du lieu en m2
+prolog.asserta("mur(4)")
+prolog.asserta("ouverture(porte)")
+prolog.asserta("desservi(electricite)")
+prolog.asserta("desservi(chauffage)")
+prolog.asserta("desservi(gaz)")
+prolog.asserta("superficie(9)")
