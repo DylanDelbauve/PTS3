4e50139a517c2c68fbea507c7f468de401e7b022
Thomas Bénas <akgx99@gmail.com>
2019-12-20 00:22:24 +0100

    déplacement des fonctions de SpaceOrRoom

--- raisonnement/placeTest.py
+++ raisonnement/placeTest.py
@@ -1,9 +1,38 @@
 from pyswip import Prolog
 import os
-import spaceOrRoom
 import facts
 prolog = Prolog
 
+#rules------------------------------------------------
+prolog.asserta("accesible() :- ouverture(X), X == porte") # une ouverture type "porte" ? si vrai il est possible d'accéder au lieu
+prolog.asserta("chauffe() :- desservi(X), X == chauffage") # dersservi par le chauffage ? si vrai la piece est chauffe
+prolog.asserta("espace() :- mur(X), X == 4, desservi(electricite), accesible()") # 4 murs & acces electricite & accesible ? si vrai c'est un espace
+prolog.asserta("piece() :- espace(), chauffe() ,superficie(X), X >= 9") # une piece ? vrai si c'est un espace & surface > 9m2
+
+
+def Space():
+    """
+       determines if the place is a space according to established rules and based on the facts
+        Returns:
+            bool: Saying whether the place is a space or not.
+    """
+    out = False
+    queryEsp= list(prolog.query("espace()"))
+    if len(queryEsp):
+       out = True
+    return out
+
+def Room():
+    """
+       determines if the place is a room according to established rules and based on the facts
+        Returns:
+            bool: Saying whether the place is a room or not.
+    """
+    out = False
+    queryRoom = list(prolog.query("piece()"))
+    if len(queryRoom):
+       out = True
+    return out
 
 def writeFacts(tab):
     """
@@ -18,8 +47,6 @@ def writeFacts(tab):
         for i in tab:
             f.write("prolog.asserta(\""+i+"\")\n")
     
-       
-
 def iskitchen():
     """
        determines if the place is a kitchen according to established rules and based on the facts
