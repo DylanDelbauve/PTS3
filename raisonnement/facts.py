72ab2cd10d437a3750977ef63704478d3719550a
Thomas Bénas <akgx99@gmail.com>
2019-12-19 09:43:44 +0100

    fact 'cellier' to 'cuisine'

--- raisonnement/facts.py
+++ raisonnement/facts.py
@@ -1,9 +1,11 @@
 from pyswip import Prolog
 prolog = Prolog
 
-prolog.asserta("libelle(cellier)") # nom du lieu
+#cusine
 prolog.asserta("mur(4)") # nombre de mur
 prolog.asserta("ouverture(porte)") # il y a une porte
-prolog.asserta("desservi(chauffage)") # il y a un acces au chauffage
+prolog.asserta("ouverture(fenetre)") # il y a une fenetre
 prolog.asserta("desservi(electricite)") # il y a un acces a l'electricite
+prolog.asserta("desservi(chauffage)") # il y a un acces au chauffage
+prolog.asserta("desservi(gaz)") # il y a un un acces au gaz
 prolog.asserta("superficie(9)") # La supérficie du lieu en m2
