40c1534a64f3fb823d5fa5767e37769cbfaf221e
Thomas Bénas <akgx99@gmail.com>
2019-12-20 00:12:02 +0100

    maj test

---
+++ raisonnement/test.py
@@ -0,0 +1,15 @@
+import placeTest
+test = placeTest
+
+tab = (
+    "mur(4)",
+    "ouverture(porte)",
+    "desservi(electricite)",
+    "desservi(chauffage)",
+    "desservi(gaz)",
+    "superficie(9)"
+)
+
+test.writeFacts(tab)
+
+print(test.spaceOrRoom.Room())
